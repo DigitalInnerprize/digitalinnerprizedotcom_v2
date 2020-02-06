58e6fec5cb7f9f2f5c0b1979dd29a3b032af9440
Martez Conner <conner.martez@gmail.com>
2020-02-06 12:25:40 -0800

    create AboutPage and WorkPage

---
+++ src/pages/work.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const WorkPage = () => (
+  <Layout>
+    <SEO title="Digital Innerprize" />
+    <h1 className="title">Work</h1>
+  </Layout>
+)
+
+export default WorkPage;
